d5b357b91f4386a9b60f2deb0a9385f382f57949
lijinyan89 <2979096145@qq.com>
2019-10-20 14:52:38 +0800

    init

---
+++ src/page/index/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lijinyan 
+ * @Date: 2019-10-20 11:54:37 
+ * @Last Modified by: lijinyan
+ * @Last Modified time: 2019-10-20 14:48:35
+ */
+
+'use strict'
+
+import('./index.css')
+
+console.log('hello index')
+
+$('body').html('hello~~~')
---
+++ src/page/login/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: lijinyan 
+ * @Date: 2019-10-20 11:54:37 
+ * @Last Modified by: lijinyan
+ * @Last Modified time: 2019-10-20 12:47:30
+ */
+
+'use strict'
+
+console.log('hello login')
+
